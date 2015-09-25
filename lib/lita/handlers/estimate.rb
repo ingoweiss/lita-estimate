cd26f4af3860be1d145f890a853bb8c387a7c102
Ingo Weiss <ingo@ingoweiss.com>
2015-09-25 19:33:58 -0400

    add 'estimate' route and simple persistence of estimate

--- lib/lita/handlers/estimate.rb
+++ lib/lita/handlers/estimate.rb
@@ -1,7 +1,13 @@
 module Lita
   module Handlers
     class Estimate < Handler
-      # insert handler code here
+
+      route /estimate (US\d+) as (\d+)/, :estimate
+
+      def estimate(response)
+        story, points = response.matches.first
+        redis.set([story,response.user.name].join(':'), points)
+      end
 
       Lita.register_handler(self)
     end
--- spec/lita/handlers/estimate_spec.rb
+++ spec/lita/handlers/estimate_spec.rb
@@ -1,4 +1,21 @@
 require "spec_helper"
 
 describe Lita::Handlers::Estimate, lita_handler: true do
+
+  describe "routing" do
+
+    it { is_expected.to route_command("estimate US123 as 1").to(:estimate) }
+
+  end
+
+  describe "estimating" do
+
+    it "should persist estimates" do
+      carl = Lita::User.create(123, name: "Carl")
+      send_command('estimate US123 as 5', :as => carl)
+      expect(subject.redis.get('US123:Carl')).to eq("5")
+    end
+
+  end
+
 end
