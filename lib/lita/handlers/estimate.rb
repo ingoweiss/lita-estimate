3a5121dc8ec65e8ee15033493cb4e52b5b506b18
Ingo Weiss <ingo@ingoweiss.com>
2015-09-25 22:23:10 -0400

    add 'show estimates' route with simple retrieval of estimation results

--- lib/lita/handlers/estimate.rb
+++ lib/lita/handlers/estimate.rb
@@ -3,10 +3,23 @@ module Lita
     class Estimate < Handler
 
       route /estimate (US\d+) as (\d+)/, :estimate
+      route /(US\d+) estimates/,         :show_estimates
 
       def estimate(response)
         story, points = response.matches.first
         redis.set([story,response.user.name].join(':'), points)
+      end
+
+      def show_estimates(response)
+        story = response.matches.flatten.first
+        estimates = []
+        redis.keys("#{story}:*").each do |key|
+          estimator = key.split(':').last
+          estimates << (estimate = redis.get(key).to_i)
+          response.reply("#{estimator}: #{estimate}")
+        end
+        average = estimates.inject{ |sum, e| sum + e }.to_f / estimates.size
+        response.reply("Average: #{average}")
       end
 
       Lita.register_handler(self)
--- spec/lita/handlers/estimate_spec.rb
+++ spec/lita/handlers/estimate_spec.rb
@@ -5,15 +5,36 @@ describe Lita::Handlers::Estimate, lita_handler: true do
   describe "routing" do
 
     it { is_expected.to route_command("estimate US123 as 1").to(:estimate) }
+    it { is_expected.to route_command("US123 estimates").to(:show_estimates) }
 
   end
 
-  describe "estimating" do
+  describe "estimate" do
 
     it "should persist estimates" do
       carl = Lita::User.create(123, name: "Carl")
       send_command('estimate US123 as 5', :as => carl)
       expect(subject.redis.get('US123:Carl')).to eq("5")
+    end
+
+  end
+
+  describe "show estimates" do
+
+    before(:each) do
+      peter = Lita::User.create(123, name: "Peter")
+      paula = Lita::User.create(234, name: "Paula")
+      send_command('estimate US123 as 5', :as => peter)
+      send_command('estimate US123 as 3', :as => paula)
+    end
+
+    it "should list estimates" do
+      send_command('US123 estimates')
+      expect(replies).to eq([
+        "Paula: 3",
+        "Peter: 5",
+        "Average: 4.0"
+      ])
     end
 
   end
