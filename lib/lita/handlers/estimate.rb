b9a0c5cdac45f3a4564779960d5615b6e7121c5e
Ingo Weiss <ingo@ingoweiss.com>
2015-09-27 09:18:05 -0400

    add help text to routes

--- lib/lita/handlers/estimate.rb
+++ lib/lita/handlers/estimate.rb
@@ -2,8 +2,8 @@ module Lita
   module Handlers
     class Estimate < Handler
 
-      route /estimate (US\d+) as (\d+)/, :estimate
-      route /(US\d+) estimates/,         :show_estimates
+      route /estimate (US\d+) as (\d+)/, :estimate,       command: true, help: {"estimate STORY_ID as FIBONACCI_NUMBER" => "Records your estimate for the story"}
+      route /(US\d+) estimates/,         :show_estimates, command: true, help: {"STORY_ID estimates" => "Lists all estimates for the story and their average"}
 
       def estimate(response)
         story, points = response.matches.first
